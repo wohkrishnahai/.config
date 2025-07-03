d5c8eada8590d8485a6223137ce0d99b71fa41d8
Krishna <krishnathakurji11@gmail.com>
2025-07-04 02:57:50 +0530

    handling large files

--- nvim/lua/krishna/plugins/treesitter.lua
+++ nvim/lua/krishna/plugins/treesitter.lua
@@ -4,12 +4,7 @@ return {
   build = ":TSUpdate",
 
   config = function()
-    local treesitter = require("nvim-treesitter.configs")
-
-    treesitter.setup({
-      highlight = { enable = true },
-      indent = { enable = true },
-
+    require("nvim-treesitter.configs").setup({
       ensure_installed = {
         "json", "toml",
         --"html", "css", "javascript",
@@ -18,6 +13,27 @@ return {
         "lua", "vimdoc", --"bash",
         "cpp", --"c",
       },
+
+      indent = { enable = true },
+
+      highlight = {
+        enable = true,
+
+        -- Disable treesitter highlighting on large files
+        disable = function(_, buf)
+             local max_filesize = 100 * 1024 -- 100 KB
+             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                if ok and stats and stats.size > max_filesize then
+                    vim.notify(
+                      "File larger than 100KB treesitter disabled for performance",
+                      vim.log.levels.WARN,
+                      { title = "Treesitter" }
+                     )
+                     return true
+              end
+       end,
+    },
+
     })
   end,
 }
