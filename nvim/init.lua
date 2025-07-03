24ee33be50b8290003c9291e8ec68488b2979a51
Krishna <krishnathakurji11@gmail.com>
2025-07-04 02:55:41 +0530

    modified Run command

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,26 @@
 require("krishna.core")
 require("krishna.lazy")
 
---for compiling and running cpp files
+-- for cpp
+-- custom Run command to take input from inputf.in file 
+-- and get output and errors in directly in outputf.in file
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "cpp",
   callback = function()
-    vim.opt_local.makeprg = "g++-15 % -o %< && gtimeout 4s ./%< <inputf.in >outputf.in"
-  end,
-},
+    -- for :Run command to only work in current cpp buffer
+    vim.api.nvim_buf_create_user_command(0, "Run", function()
+      local filepath = vim.fn.expand("%")    -- current file name
+      local filename = vim.fn.expand("%:r")  -- filename without extension
 
---assigning :Run to :silent make to get output directly in outputf.in only
-vim.api.nvim_create_user_command("Run", "silent make", {})
-)
+      local cmd = string.format(
+        "g++-15 %s -o %s 2>&1 && gtimeout 4s ./%s <inputf.in >outputf.in 2>&1",
+        filepath, filename, filename
+      )
+
+      vim.fn.system(cmd)
+      vim.cmd("checktime") -- reload output file if changed
+
+    end, {}
+    )
+  end,
+})
