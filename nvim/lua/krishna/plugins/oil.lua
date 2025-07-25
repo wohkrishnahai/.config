5dc0e502b471e44f389d2175871bb91eea088fa6
Krishna <krishnathakurji11@gmail.com>
2025-07-25 23:50:27 +0530

    feat: files view

--- nvim/lua/krishna/plugins/oil.lua
+++ nvim/lua/krishna/plugins/oil.lua
@@ -2,12 +2,15 @@ return {
     'stevearc/oil.nvim',
     dependencies = { "nvim-tree/nvim-web-devicons" },
 
-    opts = {
-        view_options = { show_hidden = true, }
-    },
-
     config = function()
-        require("oil").setup({})
+        require("oil").setup({
+            view_options = {
+                show_hidden = true,
+                is_always_hidden = function(name, _)
+                  return name == ".." or name == ".git"
+                end,
+            },
+        })
         vim.keymap.set({ "n", "x" }, "<leader>e", function() require("oil").open() end, { silent = true })
     end
 }
