ef4a39d360340652eafd444c0d95accb8bb46fc4
Krishna <krishnathakurji11@gmail.com>
2025-08-30 16:07:58 +0530

    chore: cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,7 @@ vim.g.netrw_liststyle = 3
 vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.relativenumber = true
+-- vim.opt.wrap = false
 vim.opt.cursorline = true
 vim.opt.signcolumn = "yes"
 vim.opt.tabstop = 4
@@ -14,25 +15,29 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.winborder = "rounded"
 vim.opt.clipboard = "unnamedplus"
-vim.g.mapleader = " "
+vim.opt.swapfile = false
 
+vim.g.mapleader = " "
+-- vim.keymap.set('n', '<leader>r', ':update<CR> :make<CR>')  -- Run cmd
+vim.keymap.set('n', '<leader>v', ':e $MYVIMRC<CR>')
+vim.keymap.set('n', '<leader>z', ':e ~/.config/zsh/.zshrc<CR>')
 require("krishna.lazy")
 
 -- Run cmd for cpp
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "cpp",
   callback = function()
-    vim.api.nvim_buf_create_user_command(0, "Run", function()
+    vim.keymap.set("n", "<leader>r", function()
       local filepath = vim.fn.expand("%")    -- file name
       local filename = vim.fn.expand("%:r")  -- file name w/o ext
 
+      vim.cmd("update")  -- save file before running
       local cmd = string.format(
-        "g++-15 %s -o %s 2>&1 && gtimeout 4s ./%s <inputf.in >outputf.in 2>&1",
+        "g++-15 %s -o %s 2>&1 && gtimeout 4s ./%s < inputf.in > outputf.in 2>&1",
         filepath, filename, filename
       )
       vim.fn.system(cmd)
       vim.cmd("checktime")
-    end, {}
-    )
+    end, { buffer = true })
   end,
 })
