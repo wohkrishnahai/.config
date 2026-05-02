1bcae690316fe6defbd24f5d532ac5a41688c525
Krishna <krishnathakurji11@gmail.com>
2026-05-02 22:44:17 +0530

    refractor: after

---
+++ nvim/after/ftplugin/cpp.lua
@@ -0,0 +1,34 @@
+
+-- Single line
+vim.bo.makeprg = "g++-15 -std=c++17 % -o %< && gtimeout 4s ./%< < inputf.in > outputf.in"
+
+-- vim.keymap.set("n", "<leader>r", function()
+--   local filepath = vim.fn.expand("%")
+--   local filename = vim.fn.expand("%:r")
+--
+--   vim.cmd("update")
+--   print("Compiling...")
+--
+--   local cmd = {
+--     "sh", "-c",
+--     string.format(
+--       -- "g++-15 %s -o %s 2>&1 && gtimeout 4s ./%s < inputf.in > outputf.in 2>&1",  -- w/ error redirection
+--       "g++-15 -std=c++17 %s -o %s && gtimeout 4s ./%s < inputf.in > outputf.in",
+--       filepath, filename, filename
+--     )
+--   }
+--
+--   vim.system(cmd, {text = true}, function(obj)
+--     vim.schedule(function()
+--       vim.cmd("checktime")
+--
+--       if obj.code ~= 0 then
+--         -- vim.notify("Execution Failed!", vim.log.levels.WARN)  -- w/ error redirection
+--         local error_msg = obj.stderr ~= "" and obj.stderr or "Time Limit Exceeded!"
+--         vim.notify("Execution Failed!\n" .. error_msg, vim.log.levels.WARN)
+--       else
+--         vim.notify("Execution Successful!", vim.log.levels.INFO)
+--       end
+--     end)
+--   end)
+-- end, {buffer = true, desc = "RunCmd"})
--- nvim/init.lua
+++ nvim/init.lua
@@ -23,7 +23,7 @@ vim.keymap.set("n", "<ESC>", "<Cmd>nohlsearch<CR>", {noremap = true, silent = tr
 -- vim.keymap.set("n", "<leader>r", "<Cmd>make<CR>")
 
 -- Term keymaps
-vim.keymap.set({"n", "t"}, "<leader>st", "<Cmd>botright 5split<CR> <Cmd>term<CR>i", {desc = "Small Terminal at bottom"});
+vim.keymap.set({"n", "t"}, "<leader>bt", "<Cmd>botright 5split<CR> <Cmd>term<CR>i", {desc = "Small Terminal at bottom"});
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", {desc = "Enter Normal mode in terminal"})
 
 -- Tab Switching
@@ -164,40 +164,3 @@ end
 
 vim.keymap.set("n", "<leader>pc", pack_clean)
 
-
--- RunCmd for cpp
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "cpp",
-  callback = function()
-    vim.keymap.set("n", "<leader>r", function()
-      local filepath = vim.fn.expand("%")
-      local filename = vim.fn.expand("%:r")
-
-      vim.cmd("update")
-      print("Compiling...")
-
-      local cmd = {
-        "sh", "-c",
-        string.format(
-          -- "g++-15 %s -o %s 2>&1 && gtimeout 4s ./%s < inputf.in > outputf.in 2>&1",  -- w/ error redirection
-          "g++-15 -std=c++17 %s -o %s && gtimeout 4s ./%s < inputf.in > outputf.in",
-          filepath, filename, filename
-        )
-      }
-
-      vim.system(cmd, {text = true}, function(obj)
-        vim.schedule(function()
-          vim.cmd("checktime")
-
-          if obj.code ~= 0 then
-            -- vim.notify("Execution Failed!", vim.log.levels.WARN)  -- w/ error redirection
-            local error_msg = obj.stderr ~= "" and obj.stderr or "Check Compiler Message!"
-            vim.notify("Execution Failed!\n" .. error_msg, vim.log.levels.WARN)
-          else
-            vim.notify("Execution Successful!", vim.log.levels.INFO)
-          end
-        end)
-      end)
-    end, {buffer = true, desc = "RunCmd"})
-  end,
-})
